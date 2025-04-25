66264c595207ffea7fd36fc424b2c9f92d45d6c3
mirfannsaputraa21 <m.irfansaputra0089@gmail.com>
2025-04-25 19:53:52 +0700

    kedua

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
   <HelloWorld msg="Vite + Vue" />
   <div>dwgoiwh</div>
   <div>irsyad</div>
+  <div>fiwhgw</div>
 </template>
 
 <style scoped>
